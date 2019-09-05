49b7ebd85752db116a40053137ecb790967232e5
Jón Stefánsson <jon18@ru.is>
2019-09-05 10:24:42 +0000

    Initial code

---
+++ max_int.py
@@ -0,0 +1,16 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+
+#Tékka hvort innsláttur er negatív tala
+
+#Búa til breytu til að halda utanum hæstu töluna
+
+#ef ekki þá fara í gegnum tölurnar sem slegnar voru inn og kanna hver hæst.
+max_int = 0
+
+# Fill in the missing code
+while (num_int >= 0):
+    if (num_int > max_int):
+        max_int = num_int
+
+
+print("The maximum is", max_int)    # Do not change this line
